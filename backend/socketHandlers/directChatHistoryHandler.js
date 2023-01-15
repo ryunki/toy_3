6ccb346029cd44dad354993012c4e3cd22dbdbf0
Ryunki Shin <ryunkishin@gmail.com>
2023-01-15 18:32:17 +0900

    fixed chat history not cleared for 3rd user

--- backend/socketHandlers/directChatHistoryHandler.js
+++ backend/socketHandlers/directChatHistoryHandler.js
@@ -1,16 +1,29 @@
 const Conversation = require("../models/Conversation")
+const { getSocketIo } = require("../socketServerStore")
+
 const { updateChat } = require("./updates/chat")
 
 const directChatHistoryHandler = async(socket, receiverId) =>{
+
   const senderId = socket.user._id
   try{
     const conversation = await Conversation.findOne({
       participants: {$all:[senderId, receiverId]}
     })
+    console.log("conversation found?...")
     if(conversation){
+      console.log("*yes")
       // initial update of conversation when target user is clicked
       // this socket.id is for the user who clicked the target user
       updateChat(conversation._id.toString(), socket.id)
+    }
+    else{
+      console.log("*no")
+      const io = getSocketIo()
+      io.to(socket.id).emit('direct-chat-history', {
+        participants:[],
+        messages:[]
+      })
     }
   }catch(err){
     console.log(err)
