66deb33b3888094c49890fc381f5654716e1dd09
Ryunki Shin <ryunkishin@gmail.com>
2023-01-18 14:21:30 +0900

    auto scroll to current message and some css fix

--- frontend/src/pages/MainBoard/MessageBoard/Message/MessageContents.js
+++ frontend/src/pages/MainBoard/MessageBoard/Message/MessageContents.js
@@ -1,11 +1,12 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setChat } from '../../../../store/slices/chatSlice'
 
 import './MessageContents.css'
 
 const MessageContents = () => {
+  const useChat = useRef()
   const dispatch = useDispatch()
   // const [messages, setMessages] = useState()
   const chat = useSelector(state => state.chat)
@@ -13,19 +14,22 @@ const MessageContents = () => {
   useEffect(()=>{
       //save chat to redux
       dispatch(setChat(chat))
+      console.log("REF:" , useChat.current)
+      useChat.current?.scrollIntoView({ behavior: "smooth" })
   },[chat])
   
   return (
 
-    <div className="message-contents-container">
-      <h5>
+    <div 
+      className="message-contents-container">
         {chat.messages && chat.messages.map((msg, idx)=>(
           <div key={msg._id}>
             <h5 key={msg._id}>{msg.content}</h5>
           </div>
         ))}
         {/* {messages} */}
-      </h5>
+      
+      <div ref={useChat} />
     </div>
   )
 }
