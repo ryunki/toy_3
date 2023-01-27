ab7a1fdab73303fdf9f15c34c3f25c5e170e61ae
Ryunki Shin <ryunkishin@gmail.com>
2023-01-27 15:48:18 +0900

    changed url for socketio

--- frontend/src/realtimeCommunication/socketConnection.js
+++ frontend/src/realtimeCommunication/socketConnection.js
@@ -13,9 +13,9 @@ export const socketConnection = (userData, dispatch) => {
   
   const token = userData.token
   // this is for browser
-  socket = io("http://localhost:5000",{
+  // socket = io("http://localhost:5000",{
   //this works for mobile phone and brower
-  // socket = io("http://192.168.0.5:5000",{
+  socket = io("http://192.168.0.5:5000",{
     auth:{
       token
     }
